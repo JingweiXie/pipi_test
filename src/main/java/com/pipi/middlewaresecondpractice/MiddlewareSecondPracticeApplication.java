7b02932bf5830b72e11f43a93dc813fb0cd754a8
xjw <913046572@qq.com>
2023-04-12 23:09:33 +0800

    add all

---
+++ src/main/java/com/pipi/middlewaresecondpractice/MiddlewareSecondPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pipi.middlewaresecondpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareSecondPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareSecondPracticeApplication.class, args);
+    }
+
+}
